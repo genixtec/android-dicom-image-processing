614b4c9be618067f99959b7333437c64e6eb6028
Santhosh <santhosh@genixtec.com>
2018-08-29 03:56:26 -0700

    Pan and zoom added.

--- app/src/main/java/com/genix/dicomimageprocess/MainActivity.kt
+++ app/src/main/java/com/genix/dicomimageprocess/MainActivity.kt
@@ -2,6 +2,8 @@ package com.genix.dicomimageprocess
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.davemorrissey.labs.subscaleview.ImageSource
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -27,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         val height = image.height
         val chain = Utils.applyTransformation(colorSpace, loadedDataSet,
                 image, width, height)
-        renderImage.setImageBitmap(Utils.generateBitmap(chain, image))
+        renderImage.setPanLimit(SubsamplingScaleImageView.PAN_LIMIT_INSIDE)
+        renderImage.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_INSIDE)
+        renderImage.maxScale = 8.0f
+        renderImage.setDoubleTapZoomScale(5.0f)
+        renderImage.setDoubleTapZoomStyle(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER)
+        renderImage.setDebug(BuildConfig.DEBUG)
+        renderImage.setImage(ImageSource.bitmap(Utils.generateBitmap(chain, image)!!))
     }
 }
